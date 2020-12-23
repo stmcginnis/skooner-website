951f3341fa0c9e27de971fd7dbc71f01df6ffe77
mkridzelis <53460329+mkridzelis@users.noreply.github.com>
2020-12-23 02:07:34 -0600

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
     base: '/k8dash-pages/',
-    // ...
-};
+    title: "k8dash",
+    description: null,
+    themeConfig: {
+     nav: [
+
+      {text: 'Get Started', link: '/started/'},
+      {text: 'Contribute', link: '/contribute/'},
+      {text: 'Community', link: '/community'},
+      {text: 'Brand Guidelines', link: '/brand/'}
+  
+      ]
+
+  }
+}

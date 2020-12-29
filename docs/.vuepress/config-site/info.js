93cd1f5908cd0b7293b29ec76bb2732b7904e9ce
Sue Jacobs <sjacobs@indeed.com>
2020-12-29 02:08:04 -0600

    Added site info in one js file

---
+++ docs/.vuepress/config-site/info.js
@@ -0,0 +1,18 @@
+/**
+ * Site information
+ * Data used to build site
+ */
+
+module.exports = {
+  title: "k8dash",
+  description: "A Kubernetes dashboard to manage your cluster.",
+  twitter: "",
+  author: "",
+  websiteRepo: "https://github.com/indeedeng/k8dash-site",
+  repo: "https://github.com/indeedeng/k8dash",
+  repoButtonLabel: "Star",
+  logo: "/images/k8dash-logo-stacked.png",
+  hostname: "https://k8dash.io",
+  slackInviteURL: "",
+  slackChannelURL: ""
+}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,31 @@
+/**
+ * Site data
+ */
+const siteData = require("./config-site/info");
+
 module.exports = {
-    base: '/k8dash-pages/',
-title: "k8dash",
-  description: null,
+  base: '/k8dash-pages/',
   themeConfig: {
-
+    domain: siteData.hostname,
+    twitter: siteData.twitter,
+    websiteRepo: siteData.websiteRepo,
+    repo: siteData.repo,
+    repoButtonLabel: siteData.repoButtonLabel,
+    logo: siteData.logo,
+    slackInvite: siteData.slackInviteURL,
+    slackChannel: siteData.slackChannelURL,
+    docsDir: "docs",
+    search: true,
+    displayAllHeaders: true,
+    // top bar
     nav: [
-
-      {text: 'Get Started', link: '/started/'},
       {text: 'Contribute', link: '/contribute/'},
       {text: 'Community', link: '/community'},
       {text: 'Brand Guidelines', link: '/brand/'}
-  
       ]
-
-  }
+  },
+  //title: siteData.title,
+  description: siteData.description,
+  repo: siteData.repo,
+  repoButtonLabel: siteData.repoButtonLabel
 }

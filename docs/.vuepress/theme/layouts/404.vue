a1aaf08dbbf91b2c058f0bae861b395eee85e9e9
Sue Jacobs <sjacobs@indeed.com>
2020-12-30 02:14:43 -0600

    Added slightly customized 404 error page.

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="theme-container">
+
+    <div class="theme-default-content">
+      <h1>Page Not Found</h1>
+
+      <p><img src="/images/k8dash-logo-stacked.png" alt="k8dash logo" width="15%" /></p>
+
+      <blockquote>{{ getMsg() }}</blockquote>
+
+      <RouterLink to="/">
+        Back to k8dash home
+      </RouterLink>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Home from '@theme/components/Home.vue'
+import Navbar from '@theme/components/Navbar.vue'
+import Page from '@theme/components/Page.vue'
+import Sidebar from '@theme/components/Sidebar.vue'
+import { resolveSidebarItems } from '../util'
+
+const msgs = [
+  `We couldn't find that page.`,
+  `We could not locate that page.`
+]
+
+export default {
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
+  }
+}
+</script>
